e1cf0229dbcecc6f0cad2cf7db27dbc7a48baf0d
Chris <orj.tsuk@gmail.com>
2023-02-23 15:57:25 -0500

    updating port.

--- index.js
+++ index.js
@@ -42,4 +42,4 @@ app.use(function (req, res, next) {
   res.sendStatus(404);
 });
 
-app.listen(3000, () => console.log("Listening on port 3000"));
+app.listen(process.env.PORT || 80, () => console.log("Listening on port 3000"));
