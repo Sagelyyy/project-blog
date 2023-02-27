42a53b1c400a411bf39877e63dc281f129394329
Chris Weiskopf <chris@chriscancode.io>
2023-02-27 00:11:07 -0500

    tweaking cookies

--- controllers/authController.js
+++ controllers/authController.js
@@ -27,21 +27,22 @@ exports.auth_login_post = (req, res, next) => {
         return next(err);
       }
       if (result) {
-        // JWT
+        let maxAge = 3 * 24 * 60 * 60
         jwt.sign(
           {
             email: user.email,
             admin: user.admin,
             id: user._id,
+            expiresIn: maxAge * 1000
           },
           process.env.ACCESS_TOKEN_SECRET,
           (err, token) => {
             if(err) return next(err)
-            let expireTime = new Date(Date.now() + 30 * 24 * 60 * 60 * 1000); // 30 days
             res.cookie("authorization", "Bearer " + token, {
               httpOnly: true,
+              maxAge: maxAge * 1000
             });
-            res.json({ message: "success", user });
+            res.status(201).json({ message: "success", user });
           }
         );
       } else {
