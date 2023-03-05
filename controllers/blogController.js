a4e363e6e98406051fa9d8870d2114419c6bd4ec
Chris Weiskopf <chris@chriscancode.io>
2023-03-05 16:37:57 -0500

    Removing length limits on post update.

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -3,8 +3,8 @@ const Blog = require("../models/Blog");
 
 exports.blog_get = (req, res, next) => {
   const referrer = req.headers.referer;
-  if (referrer && (referrer.includes('https://chriscancode.up.railway.app/'))) {
-    Blog.find({status: 'published'})
+  if (referrer && referrer.includes("https://chriscancode.up.railway.app/")) {
+    Blog.find({ status: "published" })
       .populate("user", "username avatar")
       .sort({ timestamp: "descending" })
       .exec(function (err, blogs) {
@@ -30,39 +30,37 @@ exports.blog_post = [
   body("title", "Invalid Title").not().isEmpty().trim().escape(),
   body("text", "Invalid Post").not().isEmpty().trim(),
   body("number", "Invalid roll").not().isEmpty().trim().escape(),
-(req, res, next) => {
-  if (req.user && req.user.admin) {
-    const errors = validationResult(req)
-    let messages = validationResult(req).array()
-    if(!errors.isEmpty()){
-      res.status(400).json(
-        {message: messages}
-      )
-      return
-    }
-    const newBlog = new Blog({
-      email: req.user.email,
-      user: req.user.id,
-      title: req.body.title,
-      text: req.body.text,
-      number: req.body.number,
-      status: req.body.status,
-      timestamp: Date.now(),
-    }).save((err, blog) => {
-      if (err) {
-        return next(err);
+  (req, res, next) => {
+    if (req.user && req.user.admin) {
+      const errors = validationResult(req);
+      let messages = validationResult(req).array();
+      if (!errors.isEmpty()) {
+        res.status(400).json({ message: messages });
+        return;
       }
-      res.json({
-        current_user: req.user.email,
-        message: "success",
-        blog,
+      const newBlog = new Blog({
+        email: req.user.email,
+        user: req.user.id,
+        title: req.body.title,
+        text: req.body.text,
+        number: req.body.number,
+        status: req.body.status,
+        timestamp: Date.now(),
+      }).save((err, blog) => {
+        if (err) {
+          return next(err);
+        }
+        res.json({
+          current_user: req.user.email,
+          message: "success",
+          blog,
+        });
       });
-    });
-  } else {
-    res.sendStatus(403);
-  }
-}
-]
+    } else {
+      res.sendStatus(403);
+    }
+  },
+];
 
 exports.blog_detail = (req, res, next) => {
   Blog.findById(req.params.id)
@@ -76,35 +74,36 @@ exports.blog_detail = (req, res, next) => {
 };
 
 exports.blog_update_put = [
-  body("title", "Invalid Title").not().isEmpty().trim().escape().isLength({max: 50}),
-  body("text", "Post").not().isEmpty().trim().escape().isLength({min: 500}),
+  body("title", "Invalid Title").not().isEmpty().trim().escape(),
+  body("text", "Post").not().isEmpty().trim().escape(),
   (req, res, next) => {
-    const errors = validationResult(req)
-    let messages = validationResult(req).array()
-    if(!errors.isEmpty()){
-      res.status(400).json(
-        {message: messages}
-      )
-      return
+    const errors = validationResult(req);
+    let messages = validationResult(req).array();
+    if (!errors.isEmpty()) {
+      res.status(400).json({ message: messages });
+      return;
     }
-  if (req.user && req.user.admin) {
-    const newBlog = new Blog({
-      user: req.user.username,
-      title: req.body.title,
-      text: req.body.text,
-      status: req.body.status,
-      _id: req.params.id,
-    });
+    if (req.user && req.user.admin) {
+      const newBlog = new Blog({
+        user: req.user.username,
+        title: req.body.title,
+        text: req.body.text,
+        status: req.body.status,
+        _id: req.params.id,
+      });
 
-    Blog.findByIdAndUpdate(req.params.id, newBlog).exec(function (err, result) {
-      if (err) return next(err);
-      res.json({ newBlog });
-    });
-  } else {
-    res.sendStatus(403);
-  }
-}
-]
+      Blog.findByIdAndUpdate(req.params.id, newBlog).exec(function (
+        err,
+        result
+      ) {
+        if (err) return next(err);
+        res.json({ newBlog });
+      });
+    } else {
+      res.sendStatus(403);
+    }
+  },
+];
 
 exports.blog_delete = (req, res, next) => {
   if (req.user && req.user.admin) {
